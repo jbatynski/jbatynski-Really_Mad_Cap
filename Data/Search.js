3099129ca3ee18b43086e451a680cc60578dabad
jbatynski <jbatynski@gmail.com>
2024-02-22 12:39:04 +0100

    final updated

--- Data/Search.js
+++ Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/ADAPTER FOR CZ KADET IN CALIBRE.htm'],s:['0','wet'],p:[['0',25],['function',0],['sa',1]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/ADAPTER FOR CZ KADET IN CALIBRE.htm'],s:['0','wet'],p:[['0',26],['further',0],['sa',2]]});
--- Resources/Scripts/require.config.js
+++ Resources/Scripts/require.config.js
@@ -1,3 +1,3 @@
 require.config({
-    urlArgs: 't=638435004541756789'
+    urlArgs: 't=638442020124771654'
 });
